3d32aa4ad93463ab1ed37515c65f206fce08d896
tom103_cp <SND\tom103_cp>
2012-05-29 16:27:18 -0700

    Added demo project

---
+++ WpfAnimatedGif/WpfAnimatedGif.Demo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfAnimatedGif.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "GIF images|*.gif";
+            if (dlg.ShowDialog() == true)
+            {
+                var converter = new ImageSourceConverter();
+                var imgSource = (ImageSource) converter.ConvertFrom(dlg.FileName);
+                ImageBehavior.SetAnimatedSource(img, imgSource);
+            }
+        }
+    }
+}
