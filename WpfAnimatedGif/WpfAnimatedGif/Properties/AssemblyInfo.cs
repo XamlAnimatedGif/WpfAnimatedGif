afe5291abd322b441ea9516ca7fc92bc59caec8f
tom103_cp <SND\tom103_cp>
2012-05-29 16:04:12 -0700

    Initial check in

---
+++ WpfAnimatedGif/WpfAnimatedGif/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WpfAnimatedGif")]
+[assembly: AssemblyDescription("A component to display animated GIF images in WPF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thomas Levesque")]
+[assembly: AssemblyProduct("WpfAnimatedGif")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a985ebe7-753b-4d73-b363-4b63d87f98b7")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsDefinition("http://wpfanimatedgif.codeplex.com", "WpfAnimatedGif")]
+[assembly: XmlnsPrefix("http://wpfanimatedgif.codeplex.com", "gif")]
