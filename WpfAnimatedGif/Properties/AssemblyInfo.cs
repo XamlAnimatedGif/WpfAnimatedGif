9dfd7f310f7b48c3b17f9fd720e7d55a5f4fd4a9
Thomas Levesque <tom103@free.fr>
2014-09-10 00:11:46 +0200

    Moved sources from old TFS branch folder to root Removed references to TFS

---
+++ WpfAnimatedGif/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("WpfAnimatedGif")]
+[assembly: AssemblyDescription("A library to display animated GIF images in WPF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thomas Levesque")]
+[assembly: AssemblyProduct("WpfAnimatedGif")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a985ebe7-753b-4d73-b363-4b63d87f98b7")]
+
+[assembly: AssemblyVersion(VersionInfo.VersionString)]
+[assembly: AssemblyFileVersion(VersionInfo.VersionString)]
+[assembly: AssemblyInformationalVersion(VersionInfo.VersionString)]
+
+[assembly: XmlnsDefinition("http://wpfanimatedgif.codeplex.com", "WpfAnimatedGif")]
+[assembly: XmlnsPrefix("http://wpfanimatedgif.codeplex.com", "gif")]
+
+// ReSharper disable CheckNamespace
+class VersionInfo
+{
+    /// <summary>
+    /// Single place to define version
+    /// </summary>
+    public const string VersionString = "1.4.4";
+}
+// ReSharper restore CheckNamespace
