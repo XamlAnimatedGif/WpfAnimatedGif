bac93010aca51847a6b0d47380b79fc87ee8d3c0
Thomas Levesque <tom103@free.fr>
2014-09-21 12:40:01 +0200

    Restored old 1.5 branch from TFS (unstable experimental)

---
+++ WpfAnimatedGif.Demo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+
+namespace WpfAnimatedGif.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGC_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+}
