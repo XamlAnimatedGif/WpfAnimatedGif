0326d13eb5cbce2ae3a2bdfe8a2b395b2bcbba6f
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-01-04 22:52:42 +0100

    Modernize build infrastructure
    Use MinVer for versioning
    Use SourceLink for source stepping
    Use .NET Core SDK to build package
    Build package during build
    Sign assembly

--- WpfAnimatedGif/Properties/AssemblyInfo.cs
+++ WpfAnimatedGif/Properties/AssemblyInfo.cs
@@ -1,38 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
-using WpfAnimatedGif.Properties;
-
-[assembly: AssemblyTitle("WpfAnimatedGif")]
-[assembly: AssemblyDescription("A library to display animated GIF images in WPF")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Thomas Levesque")]
-[assembly: AssemblyProduct("WpfAnimatedGif")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-
 [assembly: Guid("a985ebe7-753b-4d73-b363-4b63d87f98b7")]
-
-[assembly: AssemblyVersion(VersionInfo.VersionString)]
-[assembly: AssemblyFileVersion(VersionInfo.VersionString)]
-[assembly: AssemblyInformationalVersion(VersionInfo.VersionString)]
 
 [assembly: XmlnsDefinition("http://wpfanimatedgif.codeplex.com", "WpfAnimatedGif")]
 [assembly: XmlnsPrefix("http://wpfanimatedgif.codeplex.com", "gif")]
 
-[assembly: InternalsVisibleTo("WpfAnimatedGif.Demo")]
-
-namespace WpfAnimatedGif.Properties
-{
-    class VersionInfo
-    {
-        /// <summary>
-        /// Single place to define version
-        /// </summary>
-        public const string VersionString = "1.4.18";
-    }
-}
+[assembly: InternalsVisibleTo(@"WpfAnimatedGif.Demo, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d1722da3c4f3f746217be0e9110926f0ea41dd90a8d8b2e7e344fec0e1d91af0c368e54b48088d88061f7d0b87124204b6b8a5a67d5011cd0415ddb553363cc1a0603181c6f935566e607c6ae99940bf1f82c0c45df953ad5df9bdd43353eec1dd1fe3d81ff020377a65e8976ef5fa3eeb767fbba528f3e915412e72a5bc0fca")]
